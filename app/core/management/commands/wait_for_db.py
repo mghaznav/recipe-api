85bc491f178a98532a20607f11ff001a3958a530
Ali Ghaznavi <alighaznavi10@gmail.com>
2024-07-11 19:05:18 -0400

    Refactored wait_for_db command to be more robust

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,23 +1,22 @@
 import time
 
-from psycopg2 import OperationalError as Psycopg2Error
-
+from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
-
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        self.stdout.write('waiting for database...')
-        db_up = False
+        self.stdout.write('Waiting for database...')
+        db_conn = None
 
-        while db_up is False:
+        while not db_conn:
             try:
-                self.check(databases=['default'])
-                db_up = True
-            except (Psycopg2Error, OperationalError):
+                db_conn = connections['default']
+                db_conn.ensure_connection()
+            except OperationalError:
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
+
