0936d8917d6d654cfd1597698fd37b54133b9bb6
Ali Ghaznavi <alighaznavi10@gmail.com>
2024-07-11 21:50:29 -0400

    Fixed wait_for_db and its unit tests

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,28 +1,24 @@
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 
 from django.core.management import call_command
 from django.db.utils import OperationalError # noqa
-from django.test import SimpleTestCase
+from django.test import TestCase
 
 
-@patch('django.db.connections')
-class CommandTests(SimpleTestCase):
+@patch('django.db.backends.postgresql.base.DatabaseWrapper.ensure_connection')
+class CommandTests(TestCase):
+
     def test_wait_for_db_ready(self, patched_conn):
-        mock_conn = MagicMock()
-        patched_conn.__getitem__.return_value = mock_conn
-        mock_conn.ensure_connection.return_value = True
+        patched_conn.return_value = True
 
         call_command('wait_for_db')
 
-        mock_conn.ensure_connection.assert_called_once()
+        patched_conn.assert_called_once()
 
-    # @patch('time.sleep')
-    # def test_wait_for_db_delay(self, patched_sleep, patched_conn):
-    #     mock_conn = MagicMock()
-    #     patched_conn.__getitem__.return_value = mock_conn
-    #     mock_conn.ensure_connection.side_effect = [OperationalError] * 3 /
-    #     + [True]
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_conn):
+        patched_conn.side_effect = [OperationalError] * 3 + [True]
 
-    #     call_command('wait_for_db')
+        call_command('wait_for_db')
 
-    #     self.assertEqual(mock_conn.ensure_connection.call_count, 1)
+        self.assertEqual(patched_conn.call_count, 4)
